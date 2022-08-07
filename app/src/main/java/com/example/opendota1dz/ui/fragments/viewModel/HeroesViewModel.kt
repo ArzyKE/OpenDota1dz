21b3f1a0aaa468b1f2b07f69c3f812c179ddf282
adelwww <wsnezka@gmail.com>
2022-08-07 18:19:30 +0300

    передала данные

--- app/src/main/java/com/example/opendota1dz/ui/fragments/viewModel/HeroesViewModel.kt
+++ app/src/main/java/com/example/opendota1dz/ui/fragments/viewModel/HeroesViewModel.kt
@@ -1,7 +1,14 @@
 package com.example.opendota1dz.ui.fragments.viewModel
 
+import com.example.opendota1dz.data.repository.HeroesRepository
+import com.example.opendota1dz.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class HeroesViewModel {
+class HeroesViewModel @Inject constructor(
+    private val repository: HeroesRepository
+): BaseViewModel(){
+
+    fun fetchHeroes() = repository.fetchHeroes()
 }
