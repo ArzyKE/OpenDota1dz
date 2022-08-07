9d7f3d595d15a41be09d24f27799cc97d8c73a62
adelwww <wsnezka@gmail.com>
2022-08-07 18:13:56 +0300

    добавила BaseDiffUtilCallback для adapter

---
+++ app/src/main/java/com/example/opendota1dz/ui/base/BaseDiffUtilCallback.kt
@@ -0,0 +1,14 @@
+package com.example.opendota1dz.ui.base
+
+import androidx.recyclerview.widget.DiffUtil
+
+class BaseDiffUtilCallback<T : IBaseDiffModel> : DiffUtil.ItemCallback<T>() {
+
+    override fun areItemsTheSame(oldItem: T, newItem: T): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: T, newItem: T): Boolean {
+        return oldItem == newItem
+    }
+}
