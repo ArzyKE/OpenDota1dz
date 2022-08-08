b194b2bcdf97f98849835e3cfc1d155afcb45821
ArzyKE <arzymatkadyrov968@gmail.com>
2022-08-07 18:13:23 -0700

    Создал BaseRepository.kt

--- app/src/main/java/com/example/opendota1dz/data/repository/base/BaseRepository.kt
+++ app/src/main/java/com/example/opendota1dz/data/repository/base/BaseRepository.kt
@@ -1,4 +1,17 @@
 package com.example.opendota1dz.data.repository.base
 
-class BaseRepository {
+import androidx.lifecycle.liveData
+import com.example.opendota1dz.utils.Either
+import kotlinx.coroutines.Dispatchers
+import java.io.IOException
+
+open class BaseRepository {
+
+    protected fun <T> doRequest(request: suspend () -> T) = liveData(Dispatchers.IO) {
+        try {
+            emit(Either.Right(request()))
+        } catch (ioException: IOException) {
+            emit(Either.Left(ioException.localizedMessage))
+        }
+    }
 }
