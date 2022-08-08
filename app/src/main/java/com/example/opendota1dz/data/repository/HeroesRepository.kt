10efc93d924b56c29ce1845e72734180f903e8ec
ArzyKE <arzymatkadyrov968@gmail.com>
2022-08-07 18:12:11 -0700

    Сделал запрос HeroesRepository.kt

--- app/src/main/java/com/example/opendota1dz/data/repository/HeroesRepository.kt
+++ app/src/main/java/com/example/opendota1dz/data/repository/HeroesRepository.kt
@@ -1,4 +1,13 @@
 package com.example.opendota1dz.data.repository
 
-class HeroesRepository {
+import com.example.opendota1dz.data.remote.apiservices.HeroesApiServices
+import com.example.opendota1dz.data.repository.base.BaseRepository
+import javax.inject.Inject
+
+class HeroesRepository @Inject constructor(private val apiServices: HeroesApiServices) :
+    BaseRepository() {
+
+    fun fetchHeroes() = doRequest { apiServices.fetchHeroes() }
+
+
 }
